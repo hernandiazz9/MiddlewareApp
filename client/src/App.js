7f1924fc9be73e6c2d24e92578dbce7882ed5b0e
Diego Abad <diegoabad.2289@gmail.com>
2021-10-26 12:30:35 -0300

    Update Navbar (#28)

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,18 @@
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/login";
-import Home from "./components/Home";
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/login';
+import Home from './components/Home';
+import NavBar from './components/NavBar/NavBar';
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/home" component={Home} />
-        {/* <RutaPrivada exact path="/proyectos" component={Proyectos} /> */}
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/login' component={Login} />
+				<Route exact path='/home' component={Home} />
+				{/* <RutaPrivada exact path="/proyectos" component={Proyectos} /> */}
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
- 
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -6,8 +6,8 @@ function NavBar() {
 			<div className={s.nav__container__logo}>
 				<h1 className={s.nav__logo}>Middleware</h1>
 			</div>
-			<div className={s.nav__container__bridge}> 
-				<svg 
+			<div className={s.nav__container__bridge}>
+				<svg
 					xmlns='http://www.w3.org/2000/svg'
 					class='h-6 w-6'
 					fill='none'
@@ -24,7 +24,7 @@ function NavBar() {
 				<p className={s.nav__bridge__link}>Puentes de los sueños</p>
 			</div>
 			<div className={s.nav__container__links}>
-				<div className={s.nav__container__news}>
+				<div className={s.nav__container__link}>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						class='h-6 w-6'
@@ -39,9 +39,9 @@ function NavBar() {
 							d='M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
 						/>
 					</svg>
-					<p className={s.nav__news__link}>Noticias</p>
+					<p className={s.nav__container__link__text}>Noticias</p>
 				</div>
-				<div className={s.nav__container__develop}>
+				<div className={s.nav__container__link}>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						class='h-6 w-6'
@@ -56,9 +56,9 @@ function NavBar() {
 							d='M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
 						/>
 					</svg>
-					<p className={s.nav__develop__link}>Programadores</p>
+					<p className={s.nav__container__link__text}>Programadores</p>
 				</div>
-				<div className={s.nav__container__business}>
+				<div className={s.nav__container__link}>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						class='h-6 w-6'
@@ -73,9 +73,9 @@ function NavBar() {
 							d='M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
 						/>
 					</svg>
-					<p className={s.nav__business__link}>Empresas</p>
+					<p className={s.nav__container__link__text}>Empresas</p>
 				</div>
-				<div className={s.nav__container__about}>
+				<div className={s.nav__container__link}>
 					<svg
 						xmlns='http://www.w3.org/2000/svg'
 						class='h-6 w-6'
@@ -90,7 +90,7 @@ function NavBar() {
 							d='M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z'
 						/>
 					</svg>
-					<p className={s.nav__about__link}>Quines Somos</p>
+					<p className={s.nav__container__link__text}>Quines Somos</p>
 				</div>
 			</div>
 			<div className={s.responsive__menu}>
