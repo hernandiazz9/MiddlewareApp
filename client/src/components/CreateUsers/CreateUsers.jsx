44f7b8d4bc74c65fedf59c399521d6079b48843b
Maximiliano Gadea <85596286+maxigadea@users.noreply.github.com>
2021-10-26 14:26:59 -0300

    second commit, form controlled finish

--- client/src/components/CreateUsers/CreateUsers.jsx
+++ client/src/components/CreateUsers/CreateUsers.jsx
@@ -1,67 +1,141 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import { useDispatch } from 'react-redux';
+import {useHistory} from 'react-router-dom'
+ 
 
 const CreateUsers = () => {
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const [input, setInput] = useState ({
+        name: "",
+        lastname: "",
+        gmail: "",
+        github: "",
+        photo: "",
+        gender: "",
+        phone: "",
+        languages: [],
+        technologies: []
+    })
+
+    function handleChange(e) {
+        setInput({
+            ...input,
+            [e.target.name] : e.target.value
+        })
+    };
+
+    function handleSelectLanguages(e) {
+        setInput({
+            ...input,
+            languages: [...input.languages, e.target.value]
+        })
+    };
+
+    function handleSelectTechnologies(e) {
+        setInput({
+            ...input,
+            technologies: [...input.technologies, e.target.value]
+        })
+    };
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        dispatch()
+        setInput({
+            name: "",
+            lastname: "",
+            gmail: "",
+            github: "",
+            photo: "",
+            gender: "",
+            phone: "",
+            languages: [],
+            technologies: []
+        })
+        history.push('/home')
+    };
+
     return (
         <div>
-            <form>
+            <form onSubmit={e => handleSubmit(e)}>
                 <div>
                     <h1>Update your info</h1>
                 </div>
                 <div>
                     <label>Name:</label> 
                     <input type='text'
-                    value='name'
+                    value={input.name}
+                    name='name'
+                    onChange={handleChange}
                     />
                 </div>
                 <div>
                     <label>Lastname:</label> 
                     <input type='text'
-                    value='lastname'
+                    value={input.lastname}
+                    name='lastname'
+                    onChange={handleChange}
                     />
                 </div>
                 <div>
                     <label>Gmail:</label> 
                     <input type='text'
-                    value='gmail'
+                    value={input.gmail}
+                    name='gmail'
+                    onChange={handleChange}
                     />
                 </div>
                 <div>
                     <label>Github:</label> 
                     <input type='text'
-                    value='github'
+                    value={input.github}
+                    name='github'
+                    onChange={handleChange}
                     />
                 </div>
                 <div>
                     <label>Photo:</label> 
                     <input type='text'
-                    value='photograph'
+                    value={input.photo}
+                    name='photo'
+                    onChange={handleChange}
                     />
                 </div>
                 <div>
                     <label>Gender:</label> 
                     <input type='text'
-                    value='gender'
+                    value={input.gender}
+                    name='gender'
+                    onChange={handleChange}
                     />
                 </div>
                 <div>
                     <label>Phone:</label> 
                     <input type='text'
-                    value='phone'
+                    value={input.phone}
+                    name='phone'
+                    onChange={handleChange}
                     />
                 </div>
                 <div>
                     <label>Languages:</label> 
-                    <select>
-                    <option value="english">English</option>
-                    <option selected value="spanish">Spanish</option>
+                    <select onChange={e => handleSelectLanguages(e)}>
+                    <option value='english'>English</option>
+                    <option value='spanish'>Spanish</option>
+                    <option value='other'>Other</option>
                     </select>
                 </div>
                 <div>
-                    <label>Technologies:</label> 
-                    <input type='text'
-                    value='technologies'
-                    />
+                     <label>technologies:</label> 
+                    <select onChange={e => handleSelectTechnologies(e)}>
+                    <option value='javascript'>Javascript</option>
+                    <option value='java'>Java</option>
+                    <option value='phyton'>Phyton</option>
+                    <option value='c++'>C++</option>
+                    <option value='c'>C</option>
+                    </select>
+                    
                 </div>
                <div>
                     <button type='submit'>Finish</button>
