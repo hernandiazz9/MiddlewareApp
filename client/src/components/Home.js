aae7b5c3d05a2e4b498c950c032a453e5fc925fe
Ana Fernandez <86435558+anaangelicaf@users.noreply.github.com>
2021-10-26 13:46:45 -0300

    Search (#31)
    * search
    
    * prueba search

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,8 @@ import { auth } from "../firebaseConfig";
 import {  signOut } from "firebase/auth";
 import { useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
-
+import { Search } from "./Search";
+import "./styles.css/Home.css";
 const Home = () => {
   const { user } = useSelector((state) => state);
   const history = useHistory();
@@ -12,7 +13,8 @@ const Home = () => {
    
 
   return (
-    <div>
+    <div className="containerhome">
+       <Search />
       hola {user?user.displayName:'Para Comenzar, porfavor Inicia Sesión '}
       {user ? (
         <button onClick={() => signOut(auth)}>singout</button>
---
+++ client/src/components/Search.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import "./styles.css/Search.css";
+
+export const Search = () => {
+  
+  const button = "button";
+
+  const handleInputChange = (e) => {
+    
+    
+  };
+
+  const byTypeuser = (e) => {
+    
+  }
+  const byTypePublic = (e) => {
+    
+  }
+  const submit = (e) => {
+    
+  };
+
+  
+  const byTecnology = (e) => {
+   
+  }
+  const byUbication = (e) => {
+   
+  }
+  return (
+    <div className="container">
+      <form onSubmit={submit}>
+        <div className="field">
+          <input
+            type="text"
+            id="searchterm"
+            
+            onChange={handleInputChange}
+            placeholder="Realiza tu busqueda..."
+          />
+          <input className={button} type="submit" value="Buscar" />
+        </div>
+      </form>
+      <div className="field2">
+        <select className={button} name="typeUser" onChange={byTypeuser}>
+          <option value="">Tipo de usuario:</option>
+          <option value="0">Programador</option>
+          <option value="1">Empresa</option>
+          <option value="2">Administrador</option>
+        </select>
+        
+        <select className={button} name="typePublic" onChange={byTypePublic}>
+          <option value="">Tipo de Publicación:</option>
+          <option value="0">Empleo</option>
+          <option value="1">Proyectos</option>
+          <option value="2">Guias, Tutoriales</option>
+        </select>
+        <select name="tecnology" className={button} onChange={byTecnology}>
+          <option value="">Tipo de Tecnología:</option>
+          <option value="React">React</option>
+          <option value="Redux">Redux</option>
+          <option value="Css">Css</option>
+          <option value="Node">Node</option>
+          <option value="Express">Express</option>
+          <option value="Postgres">Postgres</option>
+          
+        </select>
+        <select name="ubicacion" className={button} onChange={byUbication}>
+          <option value="">Ubicación:</option>
+          <option value="Cordova">Cordova</option>
+          <option value="Buenos Aires">Buenos Aires</option>
+          <option value="Mendoza">Mendoza</option>
+          <option value="Junin">Junin</option>
+        </select>
+       
+      </div>
+    </div>
+  );
+};
