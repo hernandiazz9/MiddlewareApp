90d47cfe54b962ba494172203ab451bc46006e98
Jesuan Patermina <86751954+jesuanp@users.noreply.github.com>
2021-10-28 13:21:29 -0300

    Arreglar errores (#48)
    * .gitignore
    
    * Error arreglado

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-<<<<<<< HEAD
-import Login from "./components/Login";
-=======
 
-import Login from "./components/Login/index";
->>>>>>> c72cd485376848b2e649e1a6685d97220114e22c
+import Login from "./components/login";
 import Home from "./components/Home/Home";
 import LandingPage from './components/LandingPage/LandingPage';
 import CreateUsers from "./components/CreateUsers/CreateUsers";
@@ -19,10 +15,7 @@ function App() {
         <Route exact path="/home" component={Home} />
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/createuser" component={CreateUsers} />
-<<<<<<< HEAD
-=======
         <Route path="/companies/:id" component={CompanyDetail} />
->>>>>>> c72cd485376848b2e649e1a6685d97220114e22c
 
         {/* <RutaPrivada exact path="/proyectos" component={Proyectos} /> */}
       </Switch>
