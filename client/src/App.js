c9cba85841ce12db9d57edd6118b6148cee5dab1
Diego Abad <diegoabad.2289@gmail.com>
2021-10-29 11:13:45 -0300

    Publications/reducer/actions (#58)
    Co-authored-by: Jesuan Patermina <86751954+jesuanp@users.noreply.github.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,43 @@
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/Login/index";
-import Home from "./components/Home/Home";
-import LandingPage from "./components/LandingPage/LandingPage";
-import tokenAuth from  './components/config/token'
-import CompanyDetail from "./components/CompanyDetails/CompanyDetails";
-import ProfileUser from "./components/ProfileUser/ProfileUser";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getJuniors, getCompanies } from "./redux/actions/index.js";
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login/index';
+import Home from './components/Home/Home';
+import LandingPage from './components/LandingPage/LandingPage';
+import tokenAuth from './components/config/token';
+import CompanyDetail from './components/CompanyDetails/CompanyDetails';
+import ProfileUser from './components/ProfileUser/ProfileUser';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getJuniors, getCompanies } from './redux/actions/index.js';
 
 function App() {
-  const token = localStorage.getItem("token");
-  
-  useEffect(() => {
-    if (token) {
-      tokenAuth(token);
-    }
-  }, [token])
+	const token = localStorage.getItem('token');
 
-  // const dispatch = useDispatch();
-  //   useEffect(() => {
-  //     dispatch(getJuniors());
-  //     dispatch(getCompanies());
-  //   });
+	useEffect(() => {
+		if (token) {
+			tokenAuth(token);
+		}
+	}, [token]);
 
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/login/:type" component={Login} />
-        <Route exact path="/home" component={Home} />
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/profileuser" component={ProfileUser} />
-        <Route path="/companies/:id" component={CompanyDetail} />
+	// const dispatch = useDispatch();
+	//   useEffect(() => {
+	//     dispatch(getJuniors());
+	//     dispatch(getCompanies());
+	//   });
 
-        {/* <RutaPrivada exact path="/proyectos" component={Proyectos} /> */}
-      </Switch>
-    </Router>
-  );
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/login/:type' component={Login} />
+				<Route exact path='/home' component={Home} />
+				<Route exact path='/' component={LandingPage} />
+				<Route exact path='/profileuser' component={ProfileUser} />
+				<Route path='/companies/:id' component={CompanyDetail} />
+
+				{/* <RutaPrivada exact path="/proyectos" component={Proyectos} /> */}
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
