d802867599cbabefef1383569723cb6c5a586208
Ailín Rutchle <83827449+Ailin09@users.noreply.github.com>
2021-10-29 16:39:46 -0300

    Bootstrap card (#67)
    * estilos de card
    
    * cambioooos
    
    * mas cambios

--- client/src/components/CardsCompanies/CardsCompanies.jsx
+++ client/src/components/CardsCompanies/CardsCompanies.jsx
@@ -1,31 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./card.css";
 //import { useSelector } from 'react-redux';
 
 
 export const CardsCompanies = ({ arrayCompanies }) => {
     //const companies = useSelector((state) => state.companies);
-    
-    
+
     return (
-        <>
-            <div className='container'>
-            {arrayCompanies.length ? (
-                arrayCompanies.map((p) => (
-                    
+        <div className="d-flex justify-content-around">
+
+            {arrayCompanies.map((p) => (
+                <div className="card text-center  mx-5  bg-dark " style={{ width: " 80% " }} >
                     <Link to={`/companies/${p._id}`} key={p.name}>
-                        <div>
-                            <img src={p.photograph} alt="" />
-                        </div>
-                        <div>
-                            <p>{p.name}</p>
-                            
+
+                        <img src={p.photograph} className="card-img-top" style={{ width: " 80% " }} alt="Card image cap" />
+
+                        <div className="card-body  text-light">
+                            <p className="card-title">{p.name}</p>
+                            <p>Empresa Premium: {p.premium}</p>
                         </div>
                     </Link>
-                ))
-            ) : (    <p>Loading...</p>
-            )}
-            </div>
-        </>
+                </div >
+            ))}
+        </div >
+
+
     );
 };
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
   useEffect(() => {
     dispatch(getJuniors());
     dispatch(getCompanies());
-  },[]);
+  }, []);
   onAuthStateChanged(auth, (userFirebase) => {
     if (userFirebase) {
       if (user) return;
@@ -37,19 +37,24 @@ const Home = () => {
 
   const companies = useSelector((state) => state.companies);
   const juniors = useSelector((state) => state.juniors);
-  
+
   return (
     <div className='containerhome'>
-            <NavBar />
-            <div className='searchcards'>
-                <div className='search'>
-                    <Search />
-                </div>
-                <div className='cards'>
-                    <CardsCompanies arrayCompanies={companies} />
-                   
-                </div>
+      <NavBar />
+      <div className='d-flex '>
+        <div className='row'>
+          <Search />
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="">
+
+              <CardsCompanies arrayCompanies={companies} />
             </div>
+
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
