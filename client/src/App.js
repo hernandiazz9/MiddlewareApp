4858394042d944660a438baf541abca9c00230f2
Miguel Avalle <86264543+Miguegithub78@users.noreply.github.com>
2021-10-28 21:07:30 -0300

    Solucion login (#56)
    * signup
    
    * solucion error Login

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Login from "./components/Login";
+import Login from "./components/Login/index";
 import Home from "./components/Home/Home";
 import LandingPage from "./components/LandingPage/LandingPage";
 import tokenAuth from  './components/config/token'
