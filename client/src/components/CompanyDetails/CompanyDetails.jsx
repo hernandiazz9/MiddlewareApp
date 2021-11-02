6a35250c6798a2914f44fcf80168ab73cdc6252c
Ailín Rutchle <83827449+Ailin09@users.noreply.github.com>
2021-11-02 14:48:51 -0300

    ultimo (#104)

--- client/src/components/CompanyDetails/CompanyDetails.jsx
+++ client/src/components/CompanyDetails/CompanyDetails.jsx
@@ -7,25 +7,25 @@ import { useParams } from 'react-router-dom';
 import clienteAxios from '../../components/config/clienteAxios';
 
 export default function CompanyDetail() {
-	const { id } = useParams();
+  const { id } = useParams();
 
-	const [companies, setCompanies] = useState({});
-	const dispatch = useDispatch();
+  const [companies, setCompanies] = useState({});
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		detalles();
-	}, []);
+  useEffect(() => {
+    detalles();
+  }, []);
 
-	const detalles = async () => {
-		const data = await fetch(`http://localhost:3001/companies/${id}`);
+  const detalles = async () => {
+    const data = await fetch(`http://localhost:3001/companies/${id}`);
 
-		const companies = await data.json();
+    const companies = await data.json();
 
-		setCompanies(companies);
-		console.log('Details companies' + companies.name);
-	};
+    setCompanies(companies);
+    console.log('Details companies' + companies.name);
+  };
 
-	/*const detalles = async () => {
+  /*const detalles = async () => {
     
       try {
         const data = await clienteAxios.get(`/companies/${id}`);
@@ -38,33 +38,32 @@ export default function CompanyDetail() {
     
   };*/
 
-	return (
-		<div>
-			<div>
-				<div>
-					<h1>{companies.name}</h1>
-					<img
-						src={companies.photograph}
-						alt='Imagen no encontrada'
-						width='100px'
-						heigth='80px'
-					></img>
-					<h3>{companies.webpage}</h3>
-					<h3>{companies.gmail}</h3>
-					<h3>{companies.country}</h3>
-					<h3>{companies.state}</h3>
-					<h3>{companies.city}</h3>
-					<h3>{companies.description}</h3>
-				</div>
-			</div>
-
-			<div>
-				<Link to='/home/companies'>
-					<button className='btn btn-block btn-dark btn-outline-light'>
-						Volver
-					</button>
-				</Link>
-			</div>
-		</div>
-	);
+  return (
+    <div className='container-fluid '>
+      <div>
+        <Link to='/home/companies'>
+          <button className='btn btn-block btn-dark btn-outline-light'>
+            Volver
+          </button>
+        </Link>
+      </div>
+      <div className='row align-items-center justify-content-center'>
+        <div className='col-5 text-center'>
+          <h1>{companies.name}</h1>
+          <img
+            src={companies.photograph}
+            alt='Imagen no encontrada'
+            width='200px'
+            heigth='200px'
+          ></img>
+          <h3>{companies.webpage}</h3>
+          <h3>{companies.gmail}</h3>
+          <h3>{companies.country}</h3>
+          <h3>{companies.state}</h3>
+          <h3>{companies.city}</h3>
+          <h3>{companies.description}</h3>
+        </div>
+      </div>
+    </div>
+  );
 }
