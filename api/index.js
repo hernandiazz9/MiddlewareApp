d2f9b644eb90ac8bdffb23373d860bf4122ce1a4
Miguegithub78 <avallemiguel@hotmail.com>
2021-11-07 09:44:21 -0300

    cambios para deploy

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 const mongoose = require('mongoose');
 const { userDatabase, password} = require('./config.env/index');
 
-const PORT = 3001;
+require('dotenv').config();
 
 //Me conecto a la base de datos
 // const uri = "mongodb+srv://Tito:titoelbanbino@cluster0.khboe.mongodb.net/midleware"
@@ -10,7 +10,7 @@ mongoose.connect(`mongodb+srv://${userDatabase}:${password}@cluster0.khboe.mongo
 
 mongoose.connection.once('open', _ => {
     console.log('Database is connected');
-    app.listen(PORT, () => { console.log(`Listening in http://localhost:${PORT}/` )});
+    app.listen(process.env.PORT, () => { console.log(`Listening in http://localhost:${process.env.PORT}/` )});
 })
 
 mongoose.connection.on('error', err => {
