d0c44ab6cd5eb35eb201543b6f22feca2b0f14b5
Miguegithub78 <avallemiguel@hotmail.com>
2021-11-07 15:28:09 -0300

    cambio en clienteAxios

--- client/src/components/config/clienteAxios.js
+++ client/src/components/config/clienteAxios.js
@@ -1,7 +1,10 @@
 import axios from 'axios'
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: process.env.REACT_APP_API_URL || "http://localhost:3001"
 });
 
 export default clienteAxios;
