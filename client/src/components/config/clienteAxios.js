93bc2d792f9cfce20f76a07ecc085bbb341ddacc
Hernan Diaz Daives <62017712+hernandiazz9@users.noreply.github.com>
2021-11-08 22:03:37 -0300

    Update clienteAxios.js

--- client/src/components/config/clienteAxios.js
+++ client/src/components/config/clienteAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const clienteAxios = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://middlewareapp-new.herokuapp.com'
 });
 
 export default clienteAxios;
